5f616bdbbeb7881adf615120673a44e8b6f20566
sbarn-okstate <samuel.barney@okstate.edu>
2025-02-20 11:32:39 -0600

    commit

--- backend/src/backend-worker.js
+++ backend/src/backend-worker.js
@@ -1,37 +1,36 @@
 // backend-worker.js
-console.log('Worker loaded');
-
-import * as tf from 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.esm.js';
+const tf = require('@tensorflow/tfjs-node');  // Import TensorFlow.js for Node.js
+const { parentPort } = require('worker_threads');  // Import worker_threads
 
 console.log('TensorFlow.js module:', tf);
-console.log('TensorFlow.js version:', tf?.version);
+console.log('TensorFlow.js version:', tf.version);
 console.log('tf.tensor exists:', typeof tf.tensor);
 
-import { prepareModel, trainModel } from './model.js';
+const { prepareModel, trainModel } = require('./model.js');  // Assuming model.js is in the same folder
 
 let active_model = null;
 
-onmessage = function(event) {
-    console.log('Worker received message');
-    const { action, data } = event.data;
-    switch(action){
-      case 'prepareModel':
-        active_model = prepareModel(data);
-        break;
-      case 'trainModel':
-        console.log('Received xs:', data.xs);
-        console.log('Received ys:', data.ys);
-        
-        if (!Array.isArray(data.xs)) {
-          console.error('data.xs is not an array:', data.xs);
-        }
+parentPort.on('message', function(event) {
+  console.log('Worker received message');
+  const { action, data } = event.data;
 
-        if (active_model) {
-          trainModel(active_model, tf.tensor(data.xs), tf.tensor(data.ys));
-        }
-        break;
-      default:
-        this.postMessage('Unknown action');
-    }
-  };
+  switch(action) {
+    case 'prepareModel':
+      active_model = prepareModel(data);
+      break;
+    case 'trainModel':
+      console.log('Received xs:', data.xs);
+      console.log('Received ys:', data.ys);
 
+      if (!Array.isArray(data.xs)) {
+        console.error('data.xs is not an array:', data.xs);
+      }
+
+      if (active_model) {
+        trainModel(active_model, tf.tensor(data.xs), tf.tensor(data.ys));
+      }
+      break;
+    default:
+      parentPort.postMessage('Unknown action');
+  }
+});
