15693c8c5f66e0739c900212d6834a319d2b6acf
Mark Taylor <mark101tay@gmail.com>
2025-03-06 14:20:03 -0600

    handle work

--- frontend/src/sandbox/components/TestDiv.jsx
+++ frontend/src/sandbox/components/TestDiv.jsx
@@ -4,6 +4,9 @@ import React, { forwardRef } from "react";
 const TestDiv = forwardRef(({ name }, ref) => {
     return (
         <div ref={ref} id={name} className="testdraggable">
+            <div className="nodeHandle">
+                <p className="nodeDragText">Drag here.</p>
+            </div>
             <p>Layer type: 
                 <span>
                     <select name="type" id="layertype">
