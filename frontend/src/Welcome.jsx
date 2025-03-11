fac7e8af57cb15f1524d0c24f865438654f9f558
sbarn-okstate <samuel.barney@okstate.edu>
2025-03-11 13:50:21 -0500

    button fix

--- frontend/src/Welcome.jsx
+++ frontend/src/Welcome.jsx
@@ -1,28 +1,21 @@
 import { Link } from "react-router";
 import './Welcome.css'
 
-function useLearn() {
-    console.log("useLearn: Not yet implemented!");
-}
-
-function useSandbox() {
-    console.log("useSandbox: Not yet inplemented!");
-}
-
-function useTest() {
-    console.log("useTest: Not yet implemented!");
-}
 
 function Welcome() {
     return(
         <>
             <h1>Welcome</h1>
             <div>
-                <p><span><button onClick={() => useLearn()}>Learn</button></span> &nbsp; &nbsp; &nbsp; <span><button onClick={() => useSandbox()}>Use Sandbox</button></span></p>
+                <p>
+                    <span>
+                        <Link to="/sandbox">
+                            <button>Use Sandbox</button>
+                        </Link>
+                    </span>
+                </p>
             </div>
-            <button onClick={() => useTest()}>Test Env</button>
             <p className="footertext">Work in progress. Content is content to change.</p>
-            <Link to="/sandbox">CRAP</Link>
         </>
     )
 
